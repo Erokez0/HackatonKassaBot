cf02005ccb2d98b8c7f39e2c593fdb0a64935df9
Erokez <168299433+Erokez0@users.noreply.github.com>
2024-04-28 02:59:16 +0300

    Update Keys.py

--- Keys.py
+++ Keys.py
@@ -1,8 +1,8 @@
 # Здесь требуется ввести ключ для создания бота в Telegram
-TelegramBotKey: str = ''
+TelegramBotKey: str = '7175915690:AAEwIFQsNGS3dTY3WvJZ1ZbqSP2PTjhK1Rw'
 
 # Здесь требуется ввести SPB Merchant ID
-sbpMerchantId: str = ''
+sbpMerchantId: str = 'MA622976'
 
 # Здесь требуется ввести secretKey
-secretKey: str = ''
+secretKey: str = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNQTYyMjk3NiIsImp0aSI6ImI1OTNkODRkLTk1MWYtNGIyZi05ZGViLTcxOWExNDM4NWVmZCJ9.si-87k3Aw5GN67orgJpoyTXC0C2OpWwRCKzLogRWawU' # noqa
